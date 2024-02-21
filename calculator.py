44475ecb89f6bc13acb08de3162d013fb562924d
Tt <tejatilak26@gmail.com>
2024-02-21 12:23:50 +0530

    edited the cal.py  file

--- calculator.py
+++ calculator.py
@@ -5,19 +5,22 @@
 # Asks user for 2 operands and 1 operator
 # Returns output of this operation
 
-a=input("Enter number 1 : ")
+a=float(input("Enter number 1 : "))
 o=input("Enter operator : ")
-b=input("Enter number 2 : ")
+b=float(input("Enter number 2 : "))
 
 if o[0] in [ '+','-','*','/' ]:
     if o[0] == '+':
-        out = a + b
+        out = float(a + b)
     elif o[0] == '-':
-        out = a - b
+        out = float(a) - b
     elif o[0] == '*':
-        out = a * b
+        out = a * float(b)
     elif o[0] == '/':
-        out = a//b
+        if b == 0:
+            print("cant div with zero")
+        else:
+            out = a//b
     print("Output : ",out)
 else:
     print("Error : Invalid Operator")
